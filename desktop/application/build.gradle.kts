f1987e25f3cee143a15a860c989ee6ed35fe20ab
Lezenford <alex.v.plekhanov@gmail.com>
2026-03-23 18:03:51 +0400

    3.3.0

--- desktop/application/build.gradle.kts
+++ desktop/application/build.gradle.kts
@@ -14,7 +14,7 @@ javafx {
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
 
-version = "3.2.2"
+version = "3.3.0"
 // Configure Protobuf plugin
 protobuf {
     protoc {
